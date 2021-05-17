aba98ad09bf8aec1085858614f094c72360480d3
the.ngoc <thenn@skymapglobal.com>
2021-05-17 14:46:55 +0700

    fix

--- app/Http/Controllers/Api/Business/KhoController.php
+++ app/Http/Controllers/Api/Business/KhoController.php
@@ -65,26 +65,27 @@ class KhoController extends Controller
 
     public function duyetXuongHang($id){
             $user =Auth::user();
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
+            
+$curl = curl_init();
+        curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://mauxanhcuocsong.vn/api/xuonghang_duyet/'.$id.'?nguoi_duyet='.$user->name,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_POSTFIELDS =>'{
-                "nguoi_duyet" : '.$user->name.'
-            }',
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-            ),
-            ));
-                $response = curl_exec($curl);
-                curl_close($curl);
-                return$response;
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_POSTFIELDS =>'{
+            "nguoi_duyet" : "asc"
+        }',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+        ));
+        $response = curl_exec($curl);
+        return$response;
+        
 
     }
     /**
